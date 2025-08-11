49b3b76d7afeb9f8f210d928aa188ee4e275077a
minhaj135 <minhaj.mansuri9298@gmail.com>
2025-08-11 17:04:43 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import BannerImage from "../assets/banner.jpg"
+
+const Hero = () => {
+  return (
+    <section className="text-gray-600 body-font mt-20">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              Lorem Ipsum is simply 
+              <br className="hidden lg:inline-block" />
+              the printing and 
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+            </p>
+            <div className="flex justify-center">
+              <button className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                Button
+              </button>
+              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                Button
+              </button>
+            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src={BannerImage}
+            />
+          </div>
+        </div>
+      </section>
+  )
+}
+
+export default Hero
